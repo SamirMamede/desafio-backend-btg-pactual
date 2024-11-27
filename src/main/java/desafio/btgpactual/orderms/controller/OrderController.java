79f85ef4b073ec7b0f6cbdffcda63c9c8ea85db9
SamirMamede <anuarsamir@gmail.com>
2024-11-27 16:56:08 -0400

    Start OrderController.

---
+++ src/main/java/desafio/btgpactual/orderms/controller/OrderController.java
@@ -0,0 +1,23 @@
+package desafio.btgpactual.orderms.controller;
+
+import desafio.btgpactual.orderms.service.OrderService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OrderController {
+
+    private final OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @GetMapping("customers/{customerId}/orders")
+    public ResponseEntity<?> listOrders(@RequestParam(name = "page", defaultValue = "0") Integer page,
+                                        @RequestParam(name = "pageSize", defaultValue = "10") Integer pageSize
+                                        ){
+    }
+}
