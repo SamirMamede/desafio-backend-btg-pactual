4dcd3c4da184702322dfd4c7fd2097f47de2bc9b
SamirMamede <anuarsamir@gmail.com>
2024-12-04 23:44:11 -0400

    Test OrderController shouldReturnHttpOk.

--- src/test/java/desafio/btgpactual/orderms/controller/OrderControllerTest.java
+++ src/test/java/desafio/btgpactual/orderms/controller/OrderControllerTest.java
@@ -1,5 +1,6 @@
 package desafio.btgpactual.orderms.controller;
 
+import desafio.btgpactual.orderms.factory.OrderResponseFactory;
 import desafio.btgpactual.orderms.service.OrderService;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -7,8 +8,15 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.querydsl.QPageRequest;
+import org.springframework.http.HttpStatusCode;
+
+import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.doReturn;
 
 @ExtendWith(MockitoExtension.class)
 class OrderControllerTest {
@@ -25,6 +33,18 @@ class OrderControllerTest {
         @Test
         void shouldReturnHttpOk() {
 
+            var customerId = 1L;
+            var page = 0;
+            var pageSize = 10;
+            doReturn(OrderResponseFactory.buildWithOneItem())
+                    .when(orderService).findAllByCustomerId(anyLong(), any());
+
+            doReturn(BigDecimal.valueOf(20.50))
+                    .when(orderService).findTotalOnOrdersByCustomerId(anyLong());
+
+            var response = orderController.listOrders(customerId, page,pageSize);
+
+            assertEquals(HttpStatusCode.valueOf(200), response.getStatusCode());
         }
 
         @Test
